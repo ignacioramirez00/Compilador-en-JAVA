c798bd26630f6d4a29036b5b48d804bddd7afdbe
Matias Valin <matiasvalin98@gmail.com>
2022-09-07 17:37:18 -0300

    agregue las palabras reservadas de las constantes

--- src/EtapaLexico/PalabrasReservadas.java
+++ src/EtapaLexico/PalabrasReservadas.java
@@ -4,18 +4,21 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class PalabrasReservadas {
-    Map<Integer,String> palabras = Map.of(
-            1, "when",
-            2, "while",
-            3, "if",
-            4, "then",
-            5, "else",
-            6, "end-if",
-            7, "out",
-            8, "fun",
-            9, "return",
-            10, "break"); //when, while, if, then, else, end-if, out, fun, return, break
+    Map<Integer,String> palabras; //when, while, if, then, else, end-if, out, fun, return, break
 
     public PalabrasReservadas() {
+        palabras = Map.of(
+                1, "when",
+                2, "while",
+                3, "if",
+                4, "then",
+                5, "else",
+                6, "end-if",
+                7, "out",
+                8, "fun",
+                9, "return",
+                10, "break");
+        palabras.put(11, "ui8");
+        palabras.put(12, "f64");
     }
 }
