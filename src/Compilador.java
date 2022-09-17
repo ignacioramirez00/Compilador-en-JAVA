b69bca88d47b9a03b4a1b9c811efd2ccc2df5aab
Matias Valin <matiasvalin98@gmail.com>
2022-09-17 11:55:37 -0300

    agregue el mapa de tokens

--- src/Compilador.java
+++ src/Compilador.java
@@ -4,15 +4,38 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Compilador {
     private AnalisisLexico lexico;
     private List<Character> buffer = new ArrayList<Character>();
+    Map<Integer,String> tokens = new HashMap<>();
 
     public Compilador(){
-
+        tokens.put(1,"identificador");
+        tokens.put(2, "constante");
+        tokens.put(3, "=");
+        tokens.put(4, "=:");
+        tokens.put(5, "=!");
+        tokens.put(6, ">");
+        tokens.put(7, ">=");
+        tokens.put(8, "<");
+        tokens.put(9, "<=");
+        tokens.put(10, "cadena");
+        tokens.put(11, "+");
+        tokens.put(12, "-");
+        tokens.put(13, "*");
+        tokens.put(14, "/");
+        tokens.put(15, "(");
+        tokens.put(16, ")");
+        tokens.put(17, "{");
+        tokens.put(18, "}");
+        tokens.put(19, ",");
+        tokens.put(20, ";");
     }
+
     List<Character> crearBuffer(String txt) { // ta bom?
         String fileName = "txt"; // se podra poner un link de git?
         try {
